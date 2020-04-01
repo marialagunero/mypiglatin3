65990833217698375b7208d0df13cda82e47b6b0
Maria Lagunero <maria58@gmail.com>
2020-04-01 15:24:54 -0700

    add div for result in html

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
   $("form#piglatin").submit(function(event){
     event.preventDefault();
 
-    var user
-  })
+    var userWords = $("userInput").val();
+
+    function minusNonAlphabet(all) {
+      return all.replace(/[^a-zA-Z]/g, "");
+    };
+
+    console.log(minusNonAlphabet();
+  });
 });
